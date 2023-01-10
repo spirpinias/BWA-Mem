14cdd6fd64465db3d961574617385ce5a2855ee4
Stephen Pirpinias <stephen@codeocean.com>
2023-01-10 21:10:25 +0000

    Changes in 6 files

---
+++ code/bwa.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+source ./config.sh
+
+file_count=$(find -L ../data -name "*.fastq.gz" | wc -l)
+index_file_count=$(find -L ../data -name "*.amb" | wc -l)
+
+if [ "$index_file_count" -eq 1 ];
+then
+
+  echo "Environment has 1 Genome Index. Proceeding"
+
+  if [ "$file_count" -eq 1 ]; 
+  then
+    
+    echo "Using $num_thread Available Threads"
+    echo "Running in Single End Mode"
+
+    # Grabbing the Sequencing Files
+    file=$(find -L ../data -name "*.fastq.gz")
+    filename=$(basename -a $file)
+    file_prefix=$(get_read_prefix.py $filename)
+    
+    # Grabbing the Index Prefix
+    index_file=$(find -L ../data -name "*.amb")
+    index_file_name=$(basename -s .amb $index_file)
+
+    # Get the Path for the Index.
+    index_dir_name=$(dirname $index_file)
+
+    # Run BWA-MEM.
+    bwa mem -M -t "$num_thread" -T "$align_score" -v "$verbosity" "${index_dir_name}/${index_file_name}" "$file" | samtools view -b - > ../results/"$file_prefix".bam
+
+  elif [ "$file_count" -eq 2 ] && [ "$index_file_count" -eq 1 ]; then
+    
+    echo "Using $num_thread Available Threads"
+    echo "Running in Paired End Mode"
+    
+    file1_count=$(find -L ../data -name "*${pattern_fwd}" | wc -l)
+    file2_count=$(find -L ../data -name "*${pattern_rev}" | wc -l)
+
+    if [ "$file1_count" -ne 1 ] || [ "$file2_count" -ne 1 ]; then
+      echo -e "The patterns you are using to identify the forward and reverse reads files is not working.\npattern_fwd: ${pattern_fwd}\npattern_rev: ${pattern_rev}\nThese are the .fastq.gz files you are trying to identify\n$(find -L ../data -name "*.fastq.gz")"
+    fi
+
+    # Grabbing the File Prefix.
+    file1=$(find -L ../data -name "*${pattern_fwd}")
+    file2=$(find -L ../data -name "*${pattern_rev}")
+    filename=$(basename -a $file1)
+    file_prefix=$(get_read_prefix.py $filename)
+
+    # Grabbing the Index Prefix
+    index_file=$(find -L ../data -name "*.amb")
+    index_file_name=$(basename -s .amb $index_file)
+
+    # Get the parent directory of the index file name.
+    index_dir_name=$(dirname $index_file)
+
+    echo "File 1: $file1"
+    echo "File 2: $file2"
+    echo "File Prefix: $file_prefix"
+
+    ## Run BWA-MEM. 
+    bwa mem -M -t "$num_thread" -T "$align_score" -v "$verbosity" "${index_dir_name}/${index_file_name}" "$file1" "$file2" | samtools view -b - > ../results/"$file_prefix".bam
+    
+  else
+    echo "You have $file_count fastq files in the data folder. There should be 1 or 2."
+  fi
+  
+else
+  echo "Please ensure your environment has only 1 indexed genome."
+fi
---
+++ code/config.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+  echo "args:"
+  for i in $*; do 
+    echo $i 
+  done
+  echo ""
+fi
+
+# BWA
+
+some_fastq=$(find -L ../data -name "*.fastq.gz" | head -1)
+cores=$(get_cpu_count.py)
+
+if [ -z "${1}" ]; then
+  num_thread=$((cores * 2))
+else
+  num_thread="${1}"
+fi
+
+if [ -z "${2}" ]; then
+  pattern_fwd="_$(get_read_pattern.py "$some_fastq" "1")"
+else
+  pattern_fwd="${2}"
+fi
+
+if [ -z "${3}" ]; then
+  pattern_rev="_$(get_read_pattern.py "$some_fastq" "2")"
+else
+  pattern_rev="${3}"
+fi
+
+if [ -z "${4}" ]; then
+  align_score=25
+else
+  align_score="${4}"
+fi
+
+if [ -z "${5}" ]; then
+  verbosity=1
+else
+  verbosity="${5}"
+fi
