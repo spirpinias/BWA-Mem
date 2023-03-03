fb3749b28a14f49cace06aec4756d88204cc4d5e
Stephen Pirpinias <stephen@codeocean.com>
2023-03-03 18:50:33 +0000

    Edited bwa.sh

--- code/bwa.sh
+++ code/bwa.sh
@@ -25,7 +25,14 @@ do
     echo "Running in paired end mode."
     read_files="$input_fwd_fastq $input_rev_fastq"
   fi
-  
-  bwa mem $(echo -$o1) -t "$num_thread" -k "${min_seed_len}" -w "${band_width}" -d "${z_dropoff}" -r "${seed_split_ratio}" -c "${max_occ}" -A "${match_score}" -E "${gap_ext_pen}" -O "${gap_open_pen}" -B "${mm_penalty}" -L "${clip_pen}" -U "${unpair_pen}" -T "$align_score" -v "$verbosity" "${index_dir_name}/${index_file_name}" $read_files | samtools view -b - > ../results/"$file_prefix".bam
+
+  flags=$(echo $o1$o2)
+  flag_count=$(echo $flags | wc -c)
+
+  if [ $flag_count -gt 0 ];
+  then
+    bwa mem $(echo -$o1$o2) -t "$num_thread" -k "${min_seed_len}" -w "${band_width}" -d "${z_dropoff}" -r "${seed_split_ratio}" -c "${max_occ}" -A "${match_score}" -E "${gap_ext_pen}" -O "${gap_open_pen}" -B "${mm_penalty}" -L "${clip_pen}" -U "${unpair_pen}" -T "$align_score" -v "$verbosity" "${index_dir_name}/${index_file_name}" $read_files | samtools view -b - > ../results/"$file_prefix".bam
+  else
+    bwa mem -t "$num_thread" -k "${min_seed_len}" -w "${band_width}" -d "${z_dropoff}" -r "${seed_split_ratio}" -c "${max_occ}" -A "${match_score}" -E "${gap_ext_pen}" -O "${gap_open_pen}" -B "${mm_penalty}" -L "${clip_pen}" -U "${unpair_pen}" -T "$align_score" -v "$verbosity" "${index_dir_name}/${index_file_name}" $read_files | samtools view -b - > ../results/"$file_prefix".bam
 done
   
